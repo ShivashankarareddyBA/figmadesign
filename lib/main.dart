ac027a7fe8c7b225e1f3a9bdb29c25da149e0687
shivashankarareddy <ssr2tech@gmail.com>
2024-01-17 07:39:44 +0530

    our app now connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:figmadesign/auth_screen/auth_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp;
+
   runApp(const MyApp());
 }
 
